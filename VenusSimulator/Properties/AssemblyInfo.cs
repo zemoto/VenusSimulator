ad73d3de17158ce0a364bc5386e16045c3cbf9de
zemoto <zemoto@gmail.com>
2021-11-16 11:34:31 -0500

    Update to .NET Core 3.1

--- VenusSimulator/Properties/AssemblyInfo.cs
+++ VenusSimulator/Properties/AssemblyInfo.cs
@@ -1,14 +1,3 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
+using System.Windows;
 
-[assembly: AssemblyTitle( "VenusSimulator" )]
-[assembly: AssemblyDescription( "" )]
-[assembly: AssemblyConfiguration( "" )]
-[assembly: AssemblyCompany( "" )]
-[assembly: AssemblyProduct( "VenusSimulator" )]
-[assembly: AssemblyCopyright( "Copyright © 2020" )]
-[assembly: AssemblyTrademark( "" )]
-[assembly: AssemblyCulture( "" )]
-[assembly: ComVisible( false )]
-[assembly: AssemblyVersion( "1.0.0.0" )]
-[assembly: AssemblyFileVersion( "1.0.0.0" )]
+[assembly: ThemeInfo( ResourceDictionaryLocation.None,ResourceDictionaryLocation.SourceAssembly )]
